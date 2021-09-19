87717d6d7437a688c54c7f5d9f56abfa60341df0
Ted Summer <ted.summer2@gmail.com>
2021-09-19 12:41:37 -0600

    refactor: some improvements and instructions

--- public/script.js
+++ public/script.js
@@ -2,38 +2,56 @@ const container = document.getElementById('main-container')
 let shiftDown = false;
 let currentBox = -1;
 let numBoxes = 0
+let helpStep = -1
 
 const boxId = (boxIndex) => `box-${boxIndex}`
 
-const selectBox = (boxIndex) => {
+const setCurrentBox = (boxIndex) => {
+  const oldBox = document.getElementById(boxId(currentBox))
+  if (oldBox) {
+    oldBox.classList.remove('active-box')
+  }
   currentBox = boxIndex
+  const newBox = document.getElementById(boxId(currentBox))
+  newBox.classList.add('active-box')
+}
+
+const selectBox = (boxIndex) => {
+  setCurrentBox(boxIndex)
   const box = document.getElementById(boxId(boxIndex))
   if (box.childNodes.length == 0) {
     box.focus()
-    return
+  } else {
+    const range = document.createRange()
+    const sel = window.getSelection()
+    const lastChild = box.childNodes[box.childNodes.length - 1]
+    range.setStartAfter(lastChild)
+    range.setEndAfter(lastChild)
+    range.collapse(true)
+    sel.removeAllRanges()
+    sel.addRange(range)
+    box.focus()
   }
-  const range = document.createRange()
-  const sel = window.getSelection()
-  const lastChild = box.childNodes[box.childNodes.length - 1]
-  range.setStartAfter(lastChild)
-  range.setEndAfter(lastChild)
-  range.collapse(true)
-  sel.removeAllRanges()
-  sel.addRange(range)
-  box.focus()
+
+  box.scrollIntoView({ block: 'nearest' })
 }
 
-const addBox = () => {
+const addBox = (text) => {
   numBoxes = numBoxes + 1
-  currentBox = numBoxes - 1
-  let currentBoxCopy = currentBox
+  let thisBoxIdx = numBoxes - 1
+  // let currentBoxCopy = currentBox
   const box = document.createElement('p')
-  box.id = boxId(currentBox)
+  box.id = boxId(thisBoxIdx)
   box.setAttribute('contenteditable', 'true')
-  box.addEventListener('click', () => {currentBox = currentBoxCopy})
+  box.addEventListener('click', () => setCurrentBox(thisBoxIdx))
   box.classList.add('box')
+  if (text) {
+    box.innerText = text
+  }
   container.appendChild(box)
-  selectBox(currentBox)
+  console.log(thisBoxIdx)
+  // setTimeout(() => selectBox(currentBox), 1000)
+  selectBox(thisBoxIdx)
 }
 
 addBox()
@@ -46,30 +64,42 @@ document.addEventListener('keydown', (e) => {
   }
 })
 
+const goToNextBox = () => {
+  if (currentBox + 1 < numBoxes) {
+    selectBox(currentBox + 1)
+  } else {
+     addBox()
+  }
+}
+
+const goToPreviousBox = () => {
+  selectBox(Math.max(currentBox - 1, 0))
+}
+
 document.addEventListener('keyup', (e) => {
   if (e.keyCode == 9) {
     e.preventDefault()
     if (shiftDown) {
-      selectBox(Math.max(currentBox - 1, 0))
+      goToPreviousBox()
     } else {
-      if (currentBox + 1 < numBoxes) {
-        selectBox(currentBox + 1)
-      } else {
-         addBox()
-      }
+      goToNextBox()
     }
-    
   } else if (e.keyCode == 16) {
     shiftDown = false
   }
 })
 
-const reset = () => {
+const clearAll = () => {
   let boxes = document.querySelectorAll('.box')
   boxes = [...boxes]
   boxes.forEach(box => box.remove())
   currentBox = -1
   numBoxes = 0
+}
+
+const reset = () => {
+  clearAll()
+  helpStep = -1
   addBox()
 }
 
@@ -92,4 +122,49 @@ const download = () => {
   element.click();
 
   document.body.removeChild(element);
+}
+
+const helpMessages = [
+  [
+    `What is this thing?`,
+    `This is a tool for having conversations with yourself. If you're ever feeling off or like you need\
+ to talk to someone, this can be a helpful way to sort out why you're feeling that way.`,
+    `How do I use it?`,
+    `Imagine you're having a conversation with a friend. Start by writing how your feel or a question\
+ then press the tab key to go to the next message box. Now role play as a friend trying to help out\
+ the person who wrote the message in the box above and write your response.`,
+    `That's pretty confusing.`,
+    `Click the question mark icon again to see an example.`
+  ],
+  [
+    `I feel like eating ice cream but I'm not sure if I should`,
+    `Do you feel like you shouldn't eat ice cream or that you really don't want ice cream`,
+    `Not sure, I am hungry but have been thinking about gelato a lot recently`,
+    `If there was ice cream and gelato in front of you now, which would you choose?`,
+    `I'd probably choose the gelato, I guess that's what I really want!`
+  ]
+]
+
+const help = () => {
+  if (helpStep == -1 && getText().length > 0) {
+    if (!confirm('This will clear your current conversation. Is that ok?')) {
+      return
+    }
+  }
+
+  clearAll()
+
+  if (helpStep == helpMessages.length - 1) {
+    addBox(`That's all for the help. Press the trashcan icon to reset everything.
+
+Keyboard controls:
+tab: next box
+shift + tab: previous box`)
+    return
+  }
+
+  helpStep += 1
+  helpMessages[helpStep].forEach(message => addBox(message))
+  // set focus back to the first box
+  selectBox(0)
 }
